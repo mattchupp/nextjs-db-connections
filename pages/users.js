d2ced50244254e586e29447705053895c9a18358
mattchupp <mattlchupp@gmail.com>
2020-05-17 10:38:32 -0500

    refactored query to getServerSideProps - to grab each time page loads

--- pages/users.js
+++ pages/users.js
@@ -14,16 +14,30 @@ function Users ({ data }) {
   )
 }
 
-Users.getInitialProps = async function () {
+// Users.getInitialProps = async function () {
+//   const db = await FirebaseDb()
+//   let data = []
+//   const querySnapshot = await db.firestore().collection('users').get()
+//   querySnapshot.forEach(doc => {
+//     data.push(doc.data())
+//   })
+//
+//   return {
+//     data
+//   }
+// }
+
+export async function getServerSideProps() {
   const db = await FirebaseDb()
   let data = []
-  const querySnapshot = await db.firestore().collection('users').get()
-  querySnapshot.forEach(doc => {
-    data.push(doc.data())
+  const queryDb = await db.firestore().collection('users').get()
+  queryDb.forEach(user => {
+    data.push(user.data())
   })
-
   return {
-    data
+    props: {
+      data
+    }
   }
 }
 
